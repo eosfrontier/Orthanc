7129f2d1ba25715cc6eb525893d7329e41729b71
WafflesMcDuff <goldjd@gmail.com>
2024-05-23 22:23:20 +0200

    cleanup

--- includes/classes/Backstory.php
+++ includes/classes/Backstory.php
@@ -7,9 +7,12 @@ class Backstory
     public function get_backstory($id, $type)
     {
         if ($type == 'concept') {
-            $query = "SELECT ecc_backstory.characterID, characters.accountID as accountID, characters.character_name as name, characters.faction as faction, update_user.name AS concept_updated_by,
-            approved_user.name AS concept_approved_by, ecc_backstory.concept_updated_date, change_requester.name as concept_changes_requested_by, ecc_backstory.concept_changes_requested_date, ecc_backstory.concept_approval_date, FROM_BASE64(concept_content) as content,
-            FROM_BASE64(concept_changes) as concept_changes,  FROM_BASE64(backstory_changes) as backstory_changes, status.status_name, status.status_description, timestamp
+            $query = "SELECT ecc_backstory.characterID, characters.accountID as accountID, characters.character_name as name, 
+            characters.faction as faction, update_user.name AS concept_updated_by, approved_user.name AS concept_approved_by, 
+            ecc_backstory.concept_updated_date, change_requester.name as concept_changes_requested_by, 
+            ecc_backstory.concept_changes_requested_date, ecc_backstory.concept_approval_date, FROM_BASE64(concept_content) as content,
+            FROM_BASE64(concept_changes) as concept_changes,  FROM_BASE64(backstory_changes) as backstory_changes, status.status_name, 
+            status.status_description, timestamp
                 FROM ecc_backstory
                 LEFT join ecc_backstory_status status on (ecc_backstory.concept_status = status.id AND status.status_type = 'concept')
                 LEFT join ecc_characters characters on (ecc_backstory.characterID = characters.characterID)
@@ -19,9 +22,12 @@ class Backstory
                 WHERE ecc_backstory.characterID = $id";
         }
         if ($type == 'backstory') {
-                $query = "SELECT ecc_backstory.characterID, characters.accountID as accountID, characters.character_name as name, characters.faction as faction, update_user.name AS backstory_updated_by,
-                 ecc_backstory.backstory_updated_date, change_requester.name as backstory_changes_requested_by, ecc_backstory.backstory_changes_requested_date, approved_user.name AS backstory_approved_by, ecc_backstory.backstory_approval_date, FROM_BASE64(backstory_content) as content,
-                FROM_BASE64(concept_changes) as concept_changes,  FROM_BASE64(backstory_changes) as backstory_changes, status.status_name, status.status_description, timestamp
+            $query = "SELECT ecc_backstory.characterID, characters.accountID as accountID, characters.character_name as name, 
+            characters.faction as faction, update_user.name AS backstory_updated_by, ecc_backstory.backstory_updated_date, 
+            change_requester.name as backstory_changes_requested_by, ecc_backstory.backstory_changes_requested_date, 
+            approved_user.name AS backstory_approved_by, ecc_backstory.backstory_approval_date, FROM_BASE64(backstory_content) as content,
+            FROM_BASE64(concept_changes) as concept_changes,  FROM_BASE64(backstory_changes) as backstory_changes, status.status_name, 
+            status.status_description, timestamp
                     FROM ecc_backstory
                     LEFT join ecc_backstory_status status on (ecc_backstory.backstory_status = status.id AND status.status_type = 'backstory')
                     LEFT join ecc_characters characters on (ecc_backstory.characterID = characters.characterID)
@@ -39,9 +45,11 @@ class Backstory
     public function get_all_backstories($type)
     {
         if ($type == 'concept') {
-            $query = "SELECT ecc_backstory.characterID, characters.accountID as accountID, characters.character_name as name, characters.faction as faction, update_user.name AS concept_updated_by,
-            approved_user.name AS concept_approved_by, ecc_backstory.concept_updated_date, change_requester.name as concept_changes_requested_by, ecc_backstory.concept_changes_requested_date, ecc_backstory.concept_approval_date, FROM_BASE64(concept_content) as content,
-            FROM_BASE64(concept_changes) as concept_changes,  FROM_BASE64(backstory_changes) as backstory_changes, status.status_name, status.status_description, timestamp
+            $query = "SELECT ecc_backstory.characterID, characters.accountID as accountID, characters.character_name as name, 
+            characters.faction as faction, update_user.name AS concept_updated_by, approved_user.name AS concept_approved_by, 
+            ecc_backstory.concept_updated_date, change_requester.name as concept_changes_requested_by, ecc_backstory.concept_changes_requested_date, 
+            ecc_backstory.concept_approval_date, FROM_BASE64(concept_content) as content, FROM_BASE64(concept_changes) as concept_changes,  
+            FROM_BASE64(backstory_changes) as backstory_changes, status.status_name, status.status_description, timestamp
                 FROM ecc_backstory
                 LEFT join ecc_backstory_status status on (ecc_backstory.concept_status = status.id AND status.status_type = 'concept')
                 LEFT join ecc_characters characters on (ecc_backstory.characterID = characters.characterID)
@@ -50,10 +58,14 @@ class Backstory
                 LEFT JOIN jml_users change_requester ON (change_requester.id = ecc_backstory.concept_changes_requested_by)
 		ORDER by characters.faction ASC, characters.character_name ASC";
         }
+
         if ($type == 'backstory') {
-            $query = "SELECT ecc_backstory.characterID, characters.accountID as accountID, characters.character_name as name, characters.faction as faction, update_user.name AS backstory_updated_by,
-            ecc_backstory.backstory_updated_date, change_requester.name as backstory_changes_requested_by, ecc_backstory.backstory_changes_requested_date, approved_user.name AS backstory_approved_by, ecc_backstory.backstory_approval_date, FROM_BASE64(backstory_content) as content, FROM_BASE64(concept_content) as concept_content,
-            FROM_BASE64(concept_changes) as concept_changes,  FROM_BASE64(backstory_changes) as backstory_changes, status.status_name as backstory_status, status.status_description, timestamp
+            $query = "SELECT ecc_backstory.characterID, characters.accountID as accountID, characters.character_name as name, 
+            characters.faction as faction, update_user.name AS backstory_updated_by, ecc_backstory.backstory_updated_date, 
+            change_requester.name as backstory_changes_requested_by, ecc_backstory.backstory_changes_requested_date, 
+            approved_user.name AS backstory_approved_by, ecc_backstory.backstory_approval_date, FROM_BASE64(backstory_content) as content, 
+            FROM_BASE64(concept_content) as concept_content, FROM_BASE64(concept_changes) as concept_changes,  
+            FROM_BASE64(backstory_changes) as backstory_changes, status.status_name as backstory_status, status.status_description, timestamp
                 FROM ecc_backstory
                 LEFT join ecc_backstory_status status on (ecc_backstory.backstory_status = status.id AND status.status_type = 'backstory')
                 LEFT join ecc_characters characters on (ecc_backstory.characterID = characters.characterID)
@@ -70,7 +82,7 @@ class Backstory
 
     public function set_backstory($id, $type, $content, $user)
     {
-        $date = str_replace(': ', ':', date("Y-m-d H:i:s"   ));
+        $date = str_replace(': ', ':', date("Y-m-d H:i:s"));
         if ($type == 'concept') {
             $query = "INSERT INTO ecc_backstory (characterID, concept_content, concept_updated_by, concept_updated_date)
                 VALUES (:id, :content, :user, :update_date)
@@ -99,7 +111,7 @@ class Backstory
         #bindParam takes arguments var, replacement, type
         $base64_content = base64_encode($content);
         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-        $stmt->bindParam(':content', $base64_content , PDO::PARAM_STR);
+        $stmt->bindParam(':content', $base64_content, PDO::PARAM_STR);
         $stmt->bindParam(':user', $user, PDO::PARAM_INT);
         $stmt->bindParam(':update_date', $date, PDO::PARAM_STR);
         $res = $stmt->execute();
