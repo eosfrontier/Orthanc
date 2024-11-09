8afa2c44e96a3147898a099863355dc6c8e7af24
WafflesMcDuff <goldjd@gmail.com>
2024-11-09 22:34:22 +0100

    add concept_comment update functionality

--- v2/chars_player/backstory/_put.php
+++ v2/chars_player/backstory/_put.php
@@ -4,10 +4,10 @@ if ( !isset( $input['char_id'] ) || !isset( $input['type'] ) || !isset($input['c
 	die(json_encode("You must include 'char_id', 'type', 'user' headers, and a json encoded request body containing the content" ));
 }
 else {
-	$types = ['concept', 'backstory', 'concept_changes' ,'backstory_changes'];
+	$types = ['concept', 'backstory', 'concept_changes' ,'backstory_changes', 'concept_comment'];
 	if ( ! in_array( $input['type'], $types ) ){
 		http_response_code( 400 );
-		die(json_encode("Invalid type. 'type' must be 'concept', 'backstory', 'concept_changes', or 'backstory_changes'"));
+		die(json_encode("Invalid type. 'type' must be 'concept', 'backstory', 'concept_changes', 'concept_comment', or 'backstory_changes'"));
 	}
 	$a_result = $c_fetch->set_backstory( $input['char_id'], $input['type'], $input['content'], $input['user'] );
 	http_response_code( 200 );
