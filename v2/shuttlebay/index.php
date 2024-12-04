7ad5d19da425bf919c2b197b98e300b9d618f765
WafflesMcDuff <goldjd@gmail.com>
2024-12-04 18:32:35 +0100

    Add shuttlebay get

---
+++ v2/shuttlebay/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/orthanc/includes/include.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/orthanc/includes/token.php';
+$shuttlebay = new Shuttlebay();
+
+switch ( $method ) {
+	case 'POST':
+		http_response_code( 501 );
+		break;
+	case 'GET':
+		require_once './_get.php';
+		break;
+	case 'PATCH':
+		http_response_code( 501 );
+		break;
+	case 'OPTIONS':
+		http_response_code( 200 );
+		break;
+	default:
+		require_once './_get.php';
+		break;
+}
